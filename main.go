94cc0457835db9d1485d037b05919b43e76c3072
Reinhard Kugler <rkugler@sba-research.org>
2026-05-30 03:33:03 -0400

    systemd service

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"os/exec"
+	"syscall"
 
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/ringbuf"
@@ -28,7 +30,43 @@ func cString(buf []byte) string {
 	return string(buf)
 }
 
+func forkDaemon() error {
+	if os.Getenv(envDaemon) == "1" {
+		return nil
+	}
+	// re-exec same binary
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	cmd.Env = append(os.Environ(), envDaemon+"=1")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+
+	devNull, err := os.OpenFile("/dev/null", os.O_RDWR, 0)
+	if err != nil {
+		return err
+	}
+	defer devNull.Close()
+
+	cmd.Stdin = devNull
+	cmd.Stdout = devNull
+	cmd.Stderr = devNull
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	log.Printf("forked daemon pid=%d\n", cmd.Process.Pid)
+
+	os.Exit(0) // parent exits immediately
+	return nil
+}
+
 func main() {
+	if err := forkDaemon(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("daemon running pid=%d ppid=%d\n", os.Getpid(), os.Getppid())
+
 	var objs bpfObjects
 
 	if err := loadBpfObjects(&objs, nil); err != nil {
@@ -53,15 +91,11 @@ func main() {
 	}
 	defer rd.Close()
 
-	fmt.Println("Tracing execve() calls... Ctrl+C to stop")
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-
-	go func() {
-		<-sig
-		rd.Close()
-	}()
+	file, err := os.Create("exec.log")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
 	for {
 		record, err := rd.Read()
@@ -79,6 +113,7 @@ func main() {
 		}
 
 		fmt.Printf(
+			file,
 			"PID=%d UID=%d COMM=%s EXEC=%s\n",
 			e.Pid,
 			e.Uid,
